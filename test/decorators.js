a6b3540f7bf344f2d5199e993d08b4d0f6cc6756
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-04-12 15:21:19 +0100

    Updated @bound decorator.
    Does not return a new class and now works on methods too.

--- test/decorators.js
+++ test/decorators.js
@@ -38,6 +38,31 @@ describe('bound', function () {
         assert.equal(m(), 2);
     });
 
+    it('should not cause conflicts with multiple symbol keys', function () {
+        var symbolM = Symbol('m');
+        var symbolN = Symbol('n');
+
+        @bound
+        class B {
+            constructor (val) {
+                this.val = val;
+            }
+            [symbolM] () {
+                return this.val;
+            }
+            [symbolN] () {
+                return this.val + 1;
+            }
+        }
+        var b = new B(2);
+        var m = b[symbolM];
+        var n = b[symbolN];
+
+        assert.equal(b.val, 2);
+        assert.equal(m(), 2);
+        assert.equal(n(), 3);
+    });
+
     it('should not bind static methods', function () {
         @bound
         class B {
@@ -52,6 +77,21 @@ describe('bound', function () {
         var m = b.m;
 
         assert.equal(b.m, undefined);
-        assert.equal();
+    });
+
+    it('should bind individual methods', function () {
+        class B {
+            constructor (val) {
+                this.val = val;
+            }
+            @bound
+            m () {
+                return this.val;
+            }
+        }
+        var b = new B(2);
+        var m = b.m;
+
+        assert.equal(b.m(), 2);
     });
 });
